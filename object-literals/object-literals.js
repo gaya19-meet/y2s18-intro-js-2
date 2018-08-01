adbd27e386a16a02a5345014d87f16fb7a0a8664
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 12:15:14 +0300

    started lab 1.2 1.1 finished most of them

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,10 @@
 // Create your 'me' object literal here!
+var me = {
+	firstName : "gaya",
+	lastName : "goldenberg",
+	favoriteFoods : ["pizza" , "apple" , "pear"],
+	age : 15.5,
+	picture : "https://www.google.com/search?q=dog&client=ubuntu&hs=1Q&channel=fs&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjI0pqevcvcAhWLaFAKHaDFD4IQ_AUICigB&biw=766&bih=637"	
+}
+var a = JSON.stringify(me)
+document.write(a)
